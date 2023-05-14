de0ce2ca5e3b48c3c39e0284e8dd13d8f08b1915
ray6l <archeray01@gmail.com>
2023-05-14 03:50:49 -0400

    Remove hello world and add mui

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,32 @@
 import React from "react";
-import HelloWorld from "./HelloWorld";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Grid, Button, ButtonGroup, Typography } from "@mui/material";
 
 function Home() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<HelloWorld />} />
-        {/* Add more routes as needed */}
+        <Route
+          exact
+          path="/"
+          element={
+            <Grid container spacing={3}>
+              <Grid item xs={12} align="center">
+                <Typography variant="h2">Typing Game</Typography>
+              </Grid>
+              <Grid item xs={12} align="center">
+                <Button
+                  color="primary"
+                  variant="contained"
+                  component={Link}
+                  to="/type"
+                >
+                  Start Typing
+                </Button>
+              </Grid>
+            </Grid>
+          }
+        />
       </Routes>
     </Router>
   );
